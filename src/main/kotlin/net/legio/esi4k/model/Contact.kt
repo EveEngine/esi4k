2d630cb829ce28c159317b83241e3fb4f5bb735d
Dallas Phillips <dallasphillips24@gmail.com>
2020-04-17 10:42:20 -0700

    implementing contacts resources

---
+++ src/main/kotlin/net/legio/esi4k/model/Contact.kt
@@ -0,0 +1,14 @@
+package net.legio.esi4k.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class Contact(
+    @JsonProperty("contact_id") val contactId: Int,
+    @JsonProperty("contact_type") val contactType: ContactType,
+    @JsonProperty("label_ids") val labelIds: List<Long>? = null,
+    @JsonProperty("standing") val standing: Float
+)
+
+enum class ContactType {
+    Character, Corporation, Alliance, Faction
+}
--- src/main/kotlin/net/legio/esi4k/model/ContactNotification.kt
+++ src/main/kotlin/net/legio/esi4k/model/ContactNotification.kt
@@ -8,15 +8,4 @@ data class ContactNotification(
     @JsonProperty("standing_level") val standingLevel: Double,
     @JsonProperty("notification_id") val notificationId: Int,
     @JsonProperty("message") val message: String
-) {
-    fun standingLevelAsString(): String {
-        return when(standingLevel){
-            -10.0 -> "Terrible"
-            in -9.99..-5.00 -> "Bad"
-            in -4.99..4.99 -> "Neutral"
-            in 5.0..9.99 -> "Good"
-            10.0 -> "Excellent"
-            else -> "[-Error-]"
-        }
-    }
-}
+)
