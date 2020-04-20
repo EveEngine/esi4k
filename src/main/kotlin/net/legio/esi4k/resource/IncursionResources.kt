213808742ac44bcfbacfc322a1769ff0eb7ec1ec
Dallas Phillips <dallasphillips24@gmail.com>
2020-04-20 16:22:41 -0700

    implementing incursions resources

---
+++ src/main/kotlin/net/legio/esi4k/resource/IncursionResources.kt
@@ -0,0 +1,23 @@
+package net.legio.esi4k.resource
+
+import net.legio.esi4k.Datasource
+import net.legio.esi4k.ESIClient
+import net.legio.esi4k.ESIResponse
+import net.legio.esi4k.Version
+import net.legio.esi4k.model.Incursion
+
+class IncursionResources(esiClient: ESIClient, version: Version, datasource: Datasource): ESIResources(esiClient, version, datasource) {
+
+    fun incursionsRaw(): ESIResponse {
+        return callExecute("/incursions")
+    }
+
+    fun incursions(): ReifyResult<List<Incursion>> {
+        return incursions(incursionsRaw())
+    }
+
+    fun incursions(esiResponse: ESIResponse): ReifyResult<List<Incursion>> {
+        return responseToEntity(esiResponse)
+    }
+
+}
