f51ea782136f263538bacf95016ec2a416109f11
Dallas Phillips <dallasphillips24@gmail.com>
2020-05-02 16:36:48 -0700

    implementing location resources

---
+++ src/main/kotlin/net/legio/esi4k/model/CharacterOnline.kt
@@ -0,0 +1,10 @@
+package net.legio.esi4k.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class CharacterOnline (
+        @JsonProperty("last_login") val lastLogin: String? = null,
+        @JsonProperty("last_logout") val lastLogout: String? = null,
+        @JsonProperty("logins") val logins: Int? = null,
+        @JsonProperty("online") val online: Boolean
+)
