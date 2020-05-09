216c04e07c6eb195d6f8b9e53b80d6a85bf4eafa
Dallas Phillips <dallasphillips24@gmail.com>
2020-05-09 12:26:41 -0700

    implementing wallet resources

---
+++ src/main/kotlin/net/legio/esi4k/model/CharacterWalletTransaction.kt
@@ -0,0 +1,16 @@
+package net.legio.esi4k.model
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class CharacterWalletTransaction(
+        @JsonProperty("client_id") val clientId: Int,
+        @JsonProperty("date") val date: String,
+        @JsonProperty("is_buy") val isBuy: Boolean,
+        @JsonProperty("is_personal") val isPersonal: Boolean,
+        @JsonProperty("journal_ref_id") val journalRefId: Long,
+        @JsonProperty("location_id") val locationId: Long,
+        @JsonProperty("quantity") val quantity: Int,
+        @JsonProperty("transaction_id") val transactionId: Long,
+        @JsonProperty("type_id") val typeId: Int,
+        @JsonProperty("unit_price")val unitPrice: Double
+)
---
+++ src/main/kotlin/net/legio/esi4k/resource/WalletResources.kt
@@ -0,0 +1,98 @@
+package net.legio.esi4k.resource
+
+import net.legio.esi4k.*
+import net.legio.esi4k.model.JournalEntry
+import net.legio.esi4k.model.CharacterWalletTransaction
+import net.legio.esi4k.model.CorporationWallet
+import net.legio.esi4k.model.CorporationWalletTransaction
+
+class WalletResources(client: ESIClient, version: Version = Version.LATEST, datasource: Datasource = Datasource.TRANQUILITY): ESIResources(client, version, datasource) {
+
+    fun characterWalletRaw(characterId: Int): ESIResponse {
+        return callExecute("/characters/$characterId/waller", authorization = true)
+    }
+
+    fun characterWallet(characterId: Int): Double? {
+        return characterWallet(characterWalletRaw(characterId))
+    }
+
+    fun characterWallet(esiResponse: ESIResponse): Double? {
+        return when(esiResponse){
+            is ESISuccessResponse -> esiResponse.content.toDoubleOrNull()
+            else -> null
+        }
+    }
+
+    fun characterWalletJournalRaw(characterId: Int, page: Int = 1): ESIResponse {
+        return with(createRequest("/characters/$characterId/wallet/journal", authorization = true)){
+            this.page = page
+            execute()
+        }
+    }
+
+    fun characterWalletJournal(characterId: Int, page: Int = 1): ReifyResult<List<JournalEntry>> {
+        return characterWalletJournal(characterWalletJournalRaw(characterId, page))
+    }
+
+    fun characterWalletJournal(esiResponse: ESIResponse): ReifyResult<List<JournalEntry>> {
+        return responseToEntityList(esiResponse)
+    }
+
+    fun characterWalletTransactionsRaw(characterId: Int, from: Long? = null ): ESIResponse {
+        return with(createRequest("/characters/$characterId/wallet/transactions", authorization = true)){
+            from?.let{addQueryParam("from_id", from.toString())}
+            execute()
+        }
+    }
+
+    fun characterWalletTransactions(characterId: Int, from: Long? = null): ReifyResult<List<CharacterWalletTransaction>> {
+        return characterWalletTransactions(characterWalletTransactionsRaw(characterId, from))
+    }
+
+    fun characterWalletTransactions(esiResponse: ESIResponse): ReifyResult<List<CharacterWalletTransaction>> {
+        return responseToEntityList(esiResponse)
+    }
+
+    fun corporationWalletRaw(corporationId: Int): ESIResponse {
+        return callExecute("/corporations/$corporationId/wallets", authorization = true)
+    }
+
+    fun corporationWallet(corporationId: Int): ReifyResult<List<CorporationWallet>> {
+        return corporationWallet(corporationWalletRaw(corporationId))
+    }
+
+    fun corporationWallet(esiResponse: ESIResponse): ReifyResult<List<CorporationWallet>> {
+        return responseToEntityList(esiResponse)
+    }
+
+    fun corporationWalletDivisionJournalsRaw(corporationId: Int, division: Int, page: Int = 1): ESIResponse {
+        return with(createRequest("/corporations/$corporationId/wallets/$division/journal", authorization = true)) {
+            this.page = page
+            execute()
+        }
+    }
+
+    fun corporationWalletDivisionJournals(corporationId: Int, division: Int, page: Int = 1): ReifyResult<List<JournalEntry>> {
+        return corporationWalletDivisionJournals(corporationWalletDivisionJournalsRaw(corporationId, division, page))
+    }
+
+    fun corporationWalletDivisionJournals(esiResponse: ESIResponse): ReifyResult<List<JournalEntry>> {
+        return responseToEntityList(esiResponse)
+    }
+
+    fun corporationWalletDivisionTransactionsRaw(corporationId: Int, division: Int, fromId: Long? = null): ESIResponse {
+        return with(createRequest("/corporations/$corporationId/wallet/$division/transactions", authorization = true)) {
+            fromId?.let{addQueryParam("from_id", fromId.toString())}
+            execute()
+        }
+    }
+
+    fun corporationWalletDivisionTransactions(corporationId: Int, division: Int, fromId: Long? = null): ReifyResult<List<CorporationWalletTransaction>> {
+        return  corporationWalletDivisionTransactions( corporationWalletDivisionTransactionsRaw(corporationId, division, fromId))
+    }
+
+    fun corporationWalletDivisionTransactions(esiResponse: ESIResponse): ReifyResult<List<CorporationWalletTransaction>> {
+        return responseToEntityList(esiResponse)
+    }
+
+}
