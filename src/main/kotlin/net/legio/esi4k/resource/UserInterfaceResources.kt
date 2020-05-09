d500a704a713905db4cf665794fa5180455df1f8
Dallas Phillips <dallasphillips24@gmail.com>
2020-05-09 11:40:04 -0700

    implementing user interface resources

---
+++ src/main/kotlin/net/legio/esi4k/resource/UserInterfaceResources.kt
@@ -0,0 +1,44 @@
+package net.legio.esi4k.resource
+
+import net.legio.esi4k.*
+import net.legio.esi4k.model.MailDetails
+
+class UserInterfaceResources(client: ESIClient, version: Version = Version.LATEST, datasource: Datasource = Datasource.TRANQUILITY): ESIResources(client, version, datasource) {
+
+    fun setAutopilotWaypoint(destinationId: Int, clearOtherWaypoints: Boolean, addToBeginning: Boolean): ESIResponse {
+        return with(createRequest("/ui/auotpilot/waypoint",HttpMethod.POST, true)) {
+            addQueryParam("destination_id", destinationId.toString())
+            addQueryParam("add_to_beginning", addToBeginning.toString())
+            addQueryParam("clear_other_waypoints", clearOtherWaypoints.toString())
+            execute()
+        }
+    }
+
+    fun openContractWindow(contractId: Int): ESIResponse {
+        return with(createRequest("/ui/openwindow/contract", HttpMethod.POST, true)) {
+            addQueryParam("contract_id", contractId.toString())
+            execute()
+        }
+    }
+
+    fun openInformationWindow(targetId: Int): ESIResponse {
+        return with(createRequest("/ui/openwindow/information",HttpMethod.POST, true)){
+            addQueryParam("target_id", targetId.toString())
+            execute()
+        }
+    }
+
+    fun openMarketDetailsWindow(typeId: Int): ESIResponse {
+        return with(createRequest("/ui/openwindow/marketdetails", HttpMethod.POST, true)) {
+            addQueryParam("type_id", typeId.toString())
+            execute()
+        }
+    }
+
+    fun openNewMailWindow(mailDetails: MailDetails): ESIResponse {
+        return with(createRequest("/ui/openwindow/newmail", HttpMethod.POST, true)) {
+            body = mapper.writeValueAsString(mailDetails)
+            execute()
+        }
+    }
+}
